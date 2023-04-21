ef542204883265f635ee4ee85067d121b822c415
Marina Nam <111960712+nyangmal123@users.noreply.github.com>
2023-04-21 18:49:14 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let a = input[0].split(' ')[0];
+let b = input[0].split(' ')[1];
+
+let newA = a[2]+a[1]+a[0];
+let newB = b[2]+b[1]+b[0];
+
+console.log(Math.max(Number(newA), Number(newB)));
