2ab43808a9010b38e18dd8e496c39a0cf40d55f2
Marina Nam <111960712+nyangmal123@users.noreply.github.com>
2023-04-22 11:44:03 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,24 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let sum = 0;
+
+function check(data){
+    let setData = new Set(data[0]);
+    for (let i = 0; i < data.length -1; i++){
+        if(data[i] !== data[i+1]){
+            if(setData.has(data[i+1])){
+                return false;
+            } else {
+            setData.add(data[i+1]);
+            }
+        }
+    }
+    return true;
+}
+
+for (let i =1; i<=n; i++){
+    let data = input[i];
+    if(check(data))sum+=1;
+}
+console.log(sum);
